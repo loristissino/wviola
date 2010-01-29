0a04f8ceafea1fc54aad4c5d8ceb67a4e086c78d
loris.tissino@gmail.com <loris.tissino@gmail.com@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-01-29 14:24:42 +0000

    some cleaning

--- trunk/data/sql/lib.model.schema.sql
+++ trunk/data/sql/lib.model.schema.sql
@@ -17,3 +17,41 @@ COMMENT ON TABLE "sf_guard_user_profile" IS '';
 
 
 SET search_path TO public;
+-----------------------------------------------------------------------------
+-- picture
+-----------------------------------------------------------------------------
+
+DROP TABLE "picture" CASCADE;
+
+
+CREATE TABLE "picture"
+(
+	"id" serial  NOT NULL,
+	"path" VARCHAR(255),
+	PRIMARY KEY ("id")
+);
+
+COMMENT ON TABLE "picture" IS '';
+
+
+SET search_path TO public;
+-----------------------------------------------------------------------------
+-- description
+-----------------------------------------------------------------------------
+
+DROP TABLE "description" CASCADE;
+
+
+CREATE TABLE "description"
+(
+	"id" serial  NOT NULL,
+	"picture_id" INTEGER,
+	"text" VARCHAR(255),
+	PRIMARY KEY ("id")
+);
+
+COMMENT ON TABLE "description" IS '';
+
+
+SET search_path TO public;
+ALTER TABLE "description" ADD CONSTRAINT "description_FK_1" FOREIGN KEY ("picture_id") REFERENCES "picture" ("id");
