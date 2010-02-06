a12c2f399722aed1b5550ebc3a312979bdef58e3
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-02-06 19:33:54 +0000

    little change in schema, to save the archive_id

--- trunk/apps/frontend/modules/welcome/templates/_info.php
+++ trunk/apps/frontend/modules/welcome/templates/_info.php
@@ -1,6 +1,6 @@
 <div class="info">
    <p>
-  <?php if(!$sf_user->isAuthenticated()): ?>
+  <?php if(!$sf_user->isAuthenticated() && $sf_context->getActionName()!='signin'): ?>
         <?php echo link_to(__('Login'), '@sf_guard_signin') ?>
   <?php else: ?>
         <?php echo link_to(__('Logout'), '@sf_guard_signout') ?> - 
