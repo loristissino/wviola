ddb015ad9f5aee3c50c465ef444e95628f7cc23a
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-02-09 15:34:29 +0000

    added sourceFile class; minor fixes

---
+++ trunk/lib/wviola/SourceFile.class.php
@@ -0,0 +1,48 @@
+<?php
+
+class SourceFile extends BasicFile
+{
+	
+	const WV_DIR= '.wviola';
+	
+	protected
+		$_relativePath,
+		$_basicPath;
+
+	public function __construct($relativePath, $basename)
+	{
+		$this->_basicPath=wvConfig::get('directory_sources');
+		parent::__construct($this->getBasicPath() . $relativePath, $basename);
+	}
+	
+	public function getBasicPath()
+	{
+		return $this->_basicPath;
+	}
+	
+	public function getWvDirPath()
+	{
+		return $this->getPath() . '/' . self::WV_DIR;
+	}
+	
+	public function getWvDirIsReadable()
+	{
+		return is_dir($this->getWvDirPath()) && is_readable($this->getWvDirPath());
+	}
+	
+	public function getWvDirIsWriteable()
+	{
+		return is_dir($this->getWvDirPath()) && is_writeable($this->getWvDirPath());
+	}
+	
+	public function canWriteWvDir()
+	{
+		return is_writeable($this->getPath());
+	}
+	
+	public function getMD5Sum()
+	{
+		return md5_file($this->getFullPath());
+	}
+
+}
