9f4bcec19baba3d7456278cff83190bc9d5335fd
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-02-22 21:27:18 +0000

    refactored scan-sources task

--- trunk/lib/task/wviolaScansourcesTask.class.php
+++ trunk/lib/task/wviolaScansourcesTask.class.php
@@ -10,13 +10,14 @@ class wviolaScansourcesTask extends sfBaseTask
     // ));
 
     $this->addOptions(array(
-      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'frontend'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
       new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'propel'),
       // add your own options here
 	
-      new sfCommandOption('subdir', null, sfCommandOption::PARAMETER_OPTIONAL, 'Subdirectory name', ''),
-      new sfCommandOption('recursive', null, sfCommandOption::PARAMETER_OPTIONAL, 'whether recursion will be applied', false),
+      new sfCommandOption('subdir', null, sfCommandOption::PARAMETER_OPTIONAL, 'Subdirectory name', '/'),
+      new sfCommandOption('recursive', null, sfCommandOption::PARAMETER_OPTIONAL, 'whether recursion will be applied', 'false'),
+      new sfCommandOption('size-limit-for-md5sum', null, sfCommandOption::PARAMETER_OPTIONAL, 'size in bytes over which md5sums will not be computed (0 means no limit)', 0),
 	
     ));
 
@@ -29,12 +30,54 @@ Call it with:
 
   [php symfony wviola:scan-sources|INFO]
 EOF;
+
+	$this->_isRecursive=false;
+	$this->_sourcesDirectory='';
+	
+
   }
 
 
-  protected function ScanDirectory($sourcesDirectory, $subdir, $recursive=false)
+  protected function mustComputeMD5Sum($size)
 	{
-		$completeDirPath=Generic::getCompletePath($sourcesDirectory, $subdir);
+		if($this->_size_limit_for_md5sum==0)
+		{
+			return true;
+		}
+		return $size < $this->_size_limit_for_md5sum;
+	}
+
+	protected function processFile($subdir, $basename)
+	{
+		$file=new SourceFile($subdir, $basename);
+		
+		$this->logsection('file', $file->getFullPath(), null, 'INFO');
+		
+		$this->log($this->formatter->format('Gathering information...', 'COMMENT'));
+		$file->gatherWvInfo();
+		
+		if($this->mustComputeMD5Sum($file->getStat('size')))
+		{
+			$this->log($this->formatter->format('Computing MD5 hash...', 'COMMENT'));
+			$file->appendMD5sum();
+		}
+		else
+		{
+			$this->log($this->formatter->format('MD5 computing skipped.', 'COMMENT'));
+		}
+		
+		$file->saveWvInfoFile();
+		$this->log($this->formatter->format('Information saved.', 'INFO'));
+
+		unset($file);
+		
+	}
+
+
+
+  protected function ScanDirectory($subdir)
+	{
+		$completeDirPath=Generic::getCompletePath($this->_sourcesDirectory, $subdir);
 		
 		if(is_dir($completeDirPath))
 		{
@@ -53,30 +96,21 @@ EOF;
 				}
 				
 				$newsubdir=Generic::getCompletePath($subdir, $basename);
-				$totalpath=Generic::getCompletePath($sourcesDirectory, $newsubdir);
+				$totalpath=Generic::getCompletePath($this->_sourcesDirectory, $newsubdir);
 				
 				if (is_dir($totalpath))
 				{
-					$this->scanDirectory($sourcesDirectory, $newsubdir, $recursive);
+					if ($this->_isRecursive)
+					{
+						$this->scanDirectory($newsubdir);
+					}
 				}
 				else
 				{
 					Generic::normalizeDirName($subdir);
 					
-					$file=new SourceFile($subdir, $basename);
+					$this->processFile($subdir, $basename);
 					
-					$this->logsection('file', $file->getFullPath(), null, 'INFO');
-					
-					$this->log($this->formatter->format('Gathering information...', 'COMMENT'));
-					$file->gatherWvInfo();
-					
-					$this->log($this->formatter->format('Computing MD5 hash...', 'COMMENT'));
-					$file->appendMD5sum();
-					
-					$file->saveWvInfoFile();
-					$this->log($this->formatter->format('Saved information.', 'INFO'));
-
-					unset($file);
 				}
 			
 			}
@@ -96,26 +130,28 @@ EOF;
 
     // add your code here
 
-	$recursion=in_array($options['recursive'], array(1, 'true', 'yes', 'y')); 
+	$this->_isRecursive=Generic::positiveOption($options['recursive']); 
 	
 	$subdir=$options['subdir'];
 	Generic::normalizeDirName($subdir, '/');
 	
-	$sourcesDirectory=wvConfig::get('directory_sources');
+	$this->_sourcesDirectory=wvConfig::get('directory_sources');
 	
-	$completeDirPath=Generic::getCompletePath($sourcesDirectory, $subdir);
+	$this->_size_limit_for_md5sum=(float)$options['size-limit-for-md5sum'];
+	
+	$completeDirPath=Generic::getCompletePath($this->_sourcesDirectory, $subdir);
 	
 	$this->log($this->formatter->format(sprintf('Scanning directory: «%s»', $completeDirPath), 'COMMENT'));
-
+	$this->log($this->formatter->format(sprintf('Recursion is %s.', $this->_isRecursive? 'on': 'off'), 'COMMENT'));
 
 	try
 	{
-		$this->ScanDirectory($sourcesDirectory, $subdir, $recursion);
+		$this->ScanDirectory($subdir);
 	}
 	catch (Exception $e)
 	{
 		$this->log($this->formatter->format($e->getMessage(), 'ERROR'));
-		return 2;
+		return 1;
 	}
 	
 	return 0;
