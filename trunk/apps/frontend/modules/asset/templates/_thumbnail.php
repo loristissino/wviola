b1706d104047f0a1c9bffaf7e54e3c9aa4c9298a
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-02-28 20:57:43 +0000

    little bug fixes

--- trunk/apps/frontend/modules/asset/templates/_thumbnail.php
+++ trunk/apps/frontend/modules/asset/templates/_thumbnail.php
@@ -1,11 +1,10 @@
 <?php if ($Asset->getHasThumbnail()): ?>
-	<?php $image_tag=sprintf(
-		'<img src="%s" alt="%s" width="%d" height="%s" />',
-		url_for('asset/thumbnail?id=' . $Asset->getId() .'&sf_format=jpeg'),
-		$Asset->getAssignedTitle(),
-		$Asset->getThumbnailWidth(),
-		$Asset->getThumbnailHeight()
-		)  // We need to build this because standard image_tag function appends 'png' to the URL
+	<?php $image_tag=image_tag(url_for('asset/thumbnail?id=' . $Asset->getId() .'&sf_format=jpeg'),
+		array(
+		'size'=>sprintf('%dx%d', $Asset->getThumbnailWidth(), $Asset->getThumbnailHeight()),
+		'alt='=>$Asset->getAssignedTitle(),
+		'title'=>$Asset->getAssignedTitle(),
+		))
 	?>
 <?php else: ?>
 	<?php $image_tag=image_tag('no' . $Asset->getAssetTypeCode() . '_thumbnail', array('alt'=>__('No thumbnail available'), 'size'=>'60x45')) ?>
