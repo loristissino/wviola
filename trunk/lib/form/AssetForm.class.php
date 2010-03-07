b6db5b6282acfe0f9b931e89d5c41694f2719043
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-03-07 22:32:10 +0000

    added form for archiving assets

--- trunk/lib/form/AssetForm.class.php
+++ trunk/lib/form/AssetForm.class.php
@@ -12,5 +12,74 @@ class AssetForm extends BaseAssetForm
 {
   public function configure()
   {
+    unset(
+      $this['uniqid'],
+      $this['status'],
+      $this['archive_id'],
+      $this['asset_type'],
+      $this['source_filename'],
+      $this['source_file_date'],
+      $this['source_md5sum'],
+      $this['lowquality_md5sum'],
+      $this['has_thumbnail'],
+      $this['thumbnail_width'],
+      $this['thumbnail_height'],
+      $this['thumbnail_size'],
+      $this['user_id'],
+      $this['created_at'],
+      $this['updated_at']
+      );
+      
+      unset(
+        $this->validatorSchema['assigned_title'],
+        $this->validatorSchema['notes']
+      );
+      
+      $this->validatorSchema['assigned_title'] = new sfValidatorString(array(
+        'required' => true,
+      ));
+      
+      $this->validatorSchema['notes'] = new sfValidatorString(array(
+        'required' => true,
+      ));
+            
+//    $this->setDefault('notes', 'your notes here');
+    
   }
+  
+  public function addThumbnailWidget(SourceFile $sourceFile, sfContext $sfContext=null)
+  {
+
+    for($i=0; $i < $sourceFile->getThumbnailNb(); $i++)
+    {
+      $thumbnails[]=sprintf(
+        '<img src="%s" alt="%s" />',
+        $sfContext->getRouting()->generate(
+          'sourcethumbnail',
+          array(
+            'path'=>Generic::b64_serialize($sourceFile->getRelativePath()),
+            'basename'=>Generic::b64_serialize($sourceFile->getBasename()),
+            'number'=>$i, 
+            'sf_format'=>'jpeg')
+          ),
+        $sfContext->getI18N()->__('Thumbnail %number%', array('%number%'=>$i))
+      );
+    }
+
+    $this->widgetSchema['thumbnail'] = new sfWidgetFormSelectRadio(
+      array(
+        'choices'=>$thumbnails,
+        'label_separator'=>'&nbsp;',
+        'separator'=>'&nbsp;&nbsp;',
+        )
+    );
+    
+    
+    $this->validatorSchema['thumbnail'] = new sfValidatorInteger(array(
+      'min'=>0,
+      'max'=>$sourceFile->getThumbnailNb()-1,
+      ));
+  }
+  
 }
+
