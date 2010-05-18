4a92e0234d72e2469642f29e217900115ec28dcb
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-05-18 13:31:34 +0000

    added datepicker

--- lib/model/sfGuardUserProfile.php
+++ lib/model/sfGuardUserProfile.php
@@ -78,7 +78,7 @@ class sfGuardUserProfile extends BasesfGuardUserProfile
   
 	public function __toString()
 	{
-		return $this->getFirstName() . ' ' . $this->getLastName();
+		return $this->getUsername() . '=>' . $this->getFirstName() . ' ' . $this->getLastName();
 	}
 	
 	public function getUsername()
