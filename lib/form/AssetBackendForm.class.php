715e282cace94ad37de0619cd01dca172cd3874d
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-06-11 14:06:05 +0000

    fixed backend asset admin

---
+++ lib/form/AssetBackendForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Asset form.
+ *
+ * @package    wviola
+ * @subpackage form
+ * @author     Loris Tissino <loris.tissino@gmail.com>
+ * @version    SVN: $Id: sfPropelFormTemplate.php 10377 2008-07-21 07:10:32Z dwhittle $
+ */
+class AssetBackendForm extends BaseAssetForm
+{
+  
+}
+
