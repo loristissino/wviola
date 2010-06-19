d555ee39317830193a002909ff4146a7ae84dfb5
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-06-19 14:57:29 +0000

    integrated lightbox for photoalbum view

--- trunk/apps/frontend/modules/asset/templates/_albumviewer.php
+++ trunk/apps/frontend/modules/asset/templates/_albumviewer.php
@@ -1,17 +1,29 @@
+<?php use_javascript('prototype.js') ?>
+<?php use_javascript('scriptaculous.js?load=effects,builder') ?>
+<?php use_javascript('lightbox.js') ?>
+<?php use_stylesheet('lightbox.css') ?>
+
+
 <p><?php echo format_number_choice('[0]There are no pictures in this album.|[1]There is one picture in this album.|(1,+Inf]There are %number% pictures in this album.', array('%number%' => $PhotoalbumAsset->getPicturesCount()), $PhotoalbumAsset->getPicturesCount($session)) ?></p>
 
 <?php if(sizeof($PhotoalbumAsset->getPhotoalbumFile($session)->getFileList())>0): ?>
-<?php for($i=0; $i<$PhotoalbumAsset->getPicturesCount($session); $i++): ?>
-  <p><img src="<?php echo url_for(
+<p><?php for($i=0; $i<$PhotoalbumAsset->getPicturesCount($session); $i++): ?>
+  <?php echo link_to(
+    '<img src="' . url_for(
+      'asset/showpicture?album='. $PhotoalbumAsset->getAssetId() . 
+      '&number='. $i) . '" width="' . $PhotoalbumAsset->getPictureWidth($i, $session)/5 . '" height="' . $PhotoalbumAsset->getPictureHeight($i, $session)/5 . '">',
+    url_for(
     'asset/showpicture?album='. $PhotoalbumAsset->getAssetId() . 
     '&number='. $i
-    ) ?>"
-    width="<?php echo $PhotoalbumAsset->getPictureWidth($i, $session) ?>"
-    height="<?php echo $PhotoalbumAsset->getPictureWidth($i, $session) ?>"
-    alt="<?php echo __('Picture number %number%, original file name %name%',
-      array('%number%'=>$i, '%name%' =>$PhotoalbumAsset->getFilename($i, $session))
-      ) ?>" /><br />
-  <?php echo __('%number%) «%filename%»', array('%number%'=>$i+1, '%filename%'=>$PhotoalbumAsset->getFilename($i, $session))) ?>
-  </p>
-<?php endfor ?>
+    ),
+    array(
+      'rel'=>'lightbox[' . $PhotoalbumAsset->getAssetId(). ']',
+      'title'=>__(
+        'Picture number %number%, original file name %name%',
+        array('%number%'=>$i, '%name%' =>$PhotoalbumAsset->getFilename($i, $session))
+      )
+      )
+    )?>
+    <?php endfor ?>
+</p>
 <?php endif ?>
