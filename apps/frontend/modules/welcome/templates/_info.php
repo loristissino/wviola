cccd647d7c8af9672bbe9ba1883d0b9456a08055
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-07-13 14:57:35 +0000

    changed some links in the bottom line

--- apps/frontend/modules/welcome/templates/_info.php
+++ apps/frontend/modules/welcome/templates/_info.php
@@ -2,9 +2,11 @@
   <hr />
   <ul class="sf_admin_actions">
     <li class="sf_admin_action_search"><?php echo link_to(__('Search'), '@asset_search') ?></li>
-    <li class="sf_admin_action_sources"><?php echo link_to(__('Sources to archive'), 'source/index') ?></li>
-    <li class="sf_admin_action_binders"><?php echo link_to(__('Binders I manage'), 'binder/index') ?></li>
-    <li class="sf_admin_action_assets"><?php echo link_to(__('Assets I archived'), 'asset/index') ?></li>
+    <?php if($sf_user->hasCredential('asset_encode')): ?>
+      <li class="sf_admin_action_sources"><?php echo link_to(__('Sources to archive'), 'source/index') ?></li>
+      <li class="sf_admin_action_binders"><?php echo link_to(__('Binders I manage'), 'binder/index') ?></li>
+      <li class="sf_admin_action_assets"><?php echo link_to(__('Assets I archived'), 'asset/index') ?></li>
+    <?php endif ?>
   </ul>
 <?php endif ?>
   <hr />
