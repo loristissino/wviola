bf02dbb63570dc8e506c07196833baa4124c6bc3
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2010-10-05 07:34:19 +0000

    added link to archive for archived assets; added some missing translations

---
+++ apps/backend/modules/binder/templates/_list_th_tabular.php
@@ -0,0 +1,101 @@
+<?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_text sf_admin_list_th_id">
+  <?php if ('id' == $sort[0]): ?>
+    <?php echo link_to(__('Id', array(), 'messages'), '@binder', array('query_string' => 'sort=id&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Id', array(), 'messages'), '@binder', array('query_string' => 'sort=id&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_foreignkey sf_admin_list_th_user_id">
+  <?php if ('user_id' == $sort[0]): ?>
+    <?php echo link_to(__('User', array(), 'messages'), '@binder', array('query_string' => 'sort=user_id&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('User', array(), 'messages'), '@binder', array('query_string' => 'sort=user_id&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_foreignkey sf_admin_list_th_category_id">
+  <?php if ('category_id' == $sort[0]): ?>
+    <?php echo link_to(__('Category', array(), 'messages'), '@binder', array('query_string' => 'sort=category_id&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Category', array(), 'messages'), '@binder', array('query_string' => 'sort=category_id&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_text sf_admin_list_th_title">
+  <?php if ('title' == $sort[0]): ?>
+    <?php echo link_to(__('Title', array(), 'messages'), '@binder', array('query_string' => 'sort=title&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Title', array(), 'messages'), '@binder', array('query_string' => 'sort=title&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_text sf_admin_list_th_code">
+  <?php if ('code' == $sort[0]): ?>
+    <?php echo link_to(__('Code', array(), 'messages'), '@binder', array('query_string' => 'sort=code&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Code', array(), 'messages'), '@binder', array('query_string' => 'sort=code&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_date sf_admin_list_th_event_date">
+  <?php if ('event_date' == $sort[0]): ?>
+    <?php echo link_to(__('Event date', array(), 'messages'), '@binder', array('query_string' => 'sort=event_date&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Event date', array(), 'messages'), '@binder', array('query_string' => 'sort=event_date&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_boolean sf_admin_list_th_is_open">
+  <?php if ('is_open' == $sort[0]): ?>
+    <?php echo link_to(__('Is open', array(), 'messages'), '@binder', array('query_string' => 'sort=is_open&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Is open', array(), 'messages'), '@binder', array('query_string' => 'sort=is_open&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_foreignkey sf_admin_list_th_archive_id">
+  <?php if ('archive_id' == $sort[0]): ?>
+    <?php echo link_to(__('Archive Id', array(), 'messages'), '@binder', array('query_string' => 'sort=archive_id&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Archive Id', array(), 'messages'), '@binder', array('query_string' => 'sort=archive_id&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_date sf_admin_list_th_created_at">
+  <?php if ('created_at' == $sort[0]): ?>
+    <?php echo link_to(__('Created at', array(), 'messages'), '@binder', array('query_string' => 'sort=created_at&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Created at', array(), 'messages'), '@binder', array('query_string' => 'sort=created_at&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_date sf_admin_list_th_updated_at">
+  <?php if ('updated_at' == $sort[0]): ?>
+    <?php echo link_to(__('Updated at', array(), 'messages'), '@binder', array('query_string' => 'sort=updated_at&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+    <?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Updated at', array(), 'messages'), '@binder', array('query_string' => 'sort=updated_at&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?>
