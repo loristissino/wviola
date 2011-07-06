3e5e37d3e18eabb920812cb3962910fb1883102d
loris.tissino <loris.tissino@0b89eb16-e288-11de-b4b3-6d349e4f6f86>
2011-07-06 07:33:28 +0000

    minor updates

--- trunk/utilities/checkdistfiles.sh
+++ trunk/utilities/checkdistfiles.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd /var/wviola
+MAINDIR="$1"
+if [[ $# -ne 1 ]]; then
+  echo "Usage:"
+  echo $0 "wviola_installation_dir"
+  exit 1
+fi
+
+cd "$MAINDIR"
+
 LIST=$(find . -iname '*dist')
 for FILE in $(echo $LIST); do 
 #  echo    'Dist-file: ' $FILE
